43c43a50a73ba05c9d4f3fc943018a80d69721e9
lee-sangryeol <tkdfuf7600@gmail.com>
2021-06-13 21:54:55 +0900

    server0.2

--- controller/users/signup.js
+++ controller/users/signup.js
@@ -3,15 +3,16 @@ const { user } = require('../../models')
 module.exports = {
     post: async(req,res) => {
         console.log(req.body)
-        const userInfo = await user.findOne({
-            where : {
-                email : req.body.email,
-            }
-        })
-        if(userInfo) {
-            res.status(404)
-        }
-        else {
+        // const userInfo = await User.findOne({
+        //     where : {
+        //         email : req.body.email,
+        //     }
+        // })
+        // if(userInfo) {
+        //     res.status(404)
+        // }
+        // else 
+        {
             user.create({
                 name : req.body.name,
                 email : req.body.email,
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class user extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
+  user.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password : DataTypes.STRING,
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     group_id : DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'user',
   });
-  return User;
+  return user;
 };
