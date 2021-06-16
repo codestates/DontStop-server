66c1a46d6e2f1259dab52d26b051688df0d357cf
lee-sangryeol <tkdfuf7600@gmail.com>
2021-06-16 23:27:37 +0900

    server0.7

--- seeders/20210616020350-demo-user.js
+++ seeders/20210616020350-demo-user.js
@@ -4,10 +4,10 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('rankings', [{
       id:"2",
-      user_id: '2',
+      user_id: '0',
       user_rank: '2',
-      user_name: 'b',
-      user_time:'2',
+      user_name: '나원빈',
+      user_time:'0',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
