eba7c7d059f99a5970c8d96634a69b1731a13516
lee-sangryeol <tkdfuf7600@gmail.com>
2021-08-02 18:37:12 +0900

    fix deploy

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const usersRouter = require('./routes/user');
-const groupsRouter = require('./routes/group');
+const usersRouter = require("./routes/user");
+const groupsRouter = require("./routes/group");
 
 const app = express();
 
@@ -13,22 +13,22 @@ app.use(express.json());
 const port = 4000;
 
 app.use(
-    cors({
-        origin : ["http://localhost:3000"],
-        // origin : true,
-        methods : 'GET, POST, OPTIONS',
-        credentials : true,
-    })
+  cors({
+    origin: ["http://localhost:3000"],
+    // origin : true,
+    methods: "GET, POST, OPTIONS",
+    credentials: true,
+  })
 );
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.status(201).send('자 이제 시작 이 야 내 꿈을 내 꿈을 위 한 여행 피 캬츄')
-})
-app.use('/users', usersRouter);
-app.use('/groups', groupsRouter);
+app.get("/", (req, res) => {
+  res.status(201).send("자 이제 시작 이 야 내 꿈을 내 꿈을 위 한 여행 피 캬츄");
+});
+app.use("/users", usersRouter);
+app.use("/groups", groupsRouter);
 
 app.listen(port, () => {
-    console.log('서버가 작 동 중.')
-})
+  console.log("서버가 작 동 중.");
+});
